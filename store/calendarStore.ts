2414f527fd0c4ddf20cfc2f9545da3c92bb6c456
asnpldev <asnpldev@gmail.com>
2022-10-27 11:22:31 +0200

    feat(calendarStore): added getTotalHoursForWeek

--- store/calendarStore.ts
+++ store/calendarStore.ts
@@ -1,24 +1,43 @@
 import {defineStore} from "pinia";
 import {useFetch} from "#imports";
-import {EventInterface} from "~/types/Event.interface";
-import {CalendarInterface} from "~/types/Calendar.interface";
+import {IEvent} from "~/types/Event.interface";
+import {ICalendar} from "~/types/Calendar.interface";
+import moment from "moment/moment";
+import {EVENT_BLACKLIST_WORDS} from "~/global.config";
 
 export const useCalendarStore = defineStore('calendar', {
     state: () => ({
-        weekInterval: { start: new Date(), end: new Date() },
-
-        calendar: null,
-        selectedEvent: {} as any,
+        weekInterval: {start: new Date(), end: new Date()} as { start: Date, end: Date },
+        calendar: null as ICalendar | null,
+        selectedEvent: {} as IEvent,
     }),
     getters: {
-        getCalendar: (state): CalendarInterface => state.calendar,
-        getSelectedEvent: (state): EventInterface => state.selectedEvent,
-        getEventById: (state) => (id: string): EventInterface => {
+        getCalendar: (state): ICalendar => state.calendar,
+        getSelectedEvent: (state): IEvent => state.selectedEvent,
+        getEventById: (state) => (id: string): IEvent => {
             return state.calendar?.events.find((event) => event.id === id)
         },
-        getFollowingEvents: (state) => (id: string) => {},
-        getEventsForWeek: (state) => (id: string) => {},
-        getTotalHoursForWeek: (state) => (id: string) => {},
+        getFollowingEvents: (state) => (id: string): IEvent[] => {
+            return [];
+        },
+        getEventsForWeek: (state) => (id: string): IEvent[] => {
+            return [];
+        },
+        getTotalHoursForWeek: (state) => (id: string): string => {
+            const events = [];
+            let totalHours = 0;
+            let totalMinutes = 0;
+
+            events.forEach(event => {
+                event.event.forEach(event => {
+                    if (!EVENT_BLACKLIST_WORDS.some(word => event.title.includes(word))) {
+                        totalHours += moment(event.end).diff(moment(event.start), 'hours');
+                        totalMinutes += moment(event.end).diff(moment(event.start), 'minutes');
+                    }
+                })
+            })
+            return `${totalHours}h${totalMinutes % 60 === 0 ? '' : totalMinutes % 60}`
+        },
     },
     actions: {
         FETCH_CALENDAR: (state) => (personaId: string) => {
