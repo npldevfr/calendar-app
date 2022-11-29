51969b767c0cbee53d9a6295442000e306b29f97
asnpldev <asnpldev@gmail.com>
2022-11-29 11:45:41 +0100

    feat: tests

---
+++ tests/unit/basic.test.ts
@@ -0,0 +1,12 @@
+import {describe, expect, test} from 'vitest'
+
+describe('Composables/Utils testing', async () => {
+
+    test('Are both values are same', async () => {
+        const x = 1
+        const y = 1
+        expect(x === y).toBe(true)
+    })
+
+
+})
