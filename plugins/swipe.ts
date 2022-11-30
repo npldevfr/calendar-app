dfb32681d2633b84d8d798ac46bcda4f79f05d9c
asnpldev <asnpldev@gmail.com>
2022-11-30 08:50:15 +0100

    fix: swipe

--- plugins/swipe.ts
+++ plugins/swipe.ts
@@ -4,5 +4,9 @@ import { defineNuxtPlugin } from '#app'
 import Vue3TouchEvents from "vue3-touch-events";
 
 export default defineNuxtPlugin((nuxt) => {
-    nuxt.vueApp.use(Vue3TouchEvents);
+    nuxt.vueApp.use(Vue3TouchEvents, {
+        swipeTolerance: 10,
+    });
+
+
 })
